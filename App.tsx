6dbf08d511315bb9491a14fd6057bd9764c4240a
Renato Ide Ogata <renatoogata95@gmail.com>
2023-04-25 18:15:01 -0300

    For now ignoring Chebox native base error

--- App.tsx
+++ App.tsx
@@ -9,6 +9,11 @@ import { AuthContextProvider } from '@contexts/AuthContext';
 import { THEME } from '@theme/index';
 import { Loading } from '@components/Loading';
 
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs([
+  "We can not support a function callback. See Github Issues for details https://github.com/adobe/react-spectrum/issues/2320",
+]);
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
 
